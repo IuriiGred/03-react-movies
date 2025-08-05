507b2772e76af007ea6c3331854ce641116607d0
IuriiGred <gredasoviurii@gmail.com>
2025-08-05 13:10:30 +0200

    try to fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,8 +2,8 @@ import './App.module.css';
 import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 
-import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import Loader from '../Loader/Loader';
 import SearchBar from '../SearchBar/SearchBar';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import MovieModal from '../MovieModal/MovieModal';
